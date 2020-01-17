66e0c589b27e7b325ef9b279423d280cb525e303
robert Salazar <11rsahome@gmail.com>
2020-01-17 13:33:46 +0100

    [proyect] add folders

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+    //para hacer una modificacion a la base de datos 
+    // Creamos unas constantes y adonde a punta la ruta
+    define('URL', 'http://localhost/ProyectoBlogMedida/');
+    //tablas nombres de datos y contraseña
+    define('HOST', 'localhost:3308');
+    define('DB', 'databaseblog');
+    define('USER', 'root');
+    define('PASSWORD', "");
+    define('CHARSET', 'utf8mb4');
+?>
